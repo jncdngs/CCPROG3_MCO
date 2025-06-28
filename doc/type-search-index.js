e335a6c5f128e9617e83d70dbf3c676d5243617d
jncdngs <jonchesterdonguines07@gmail.com>
2025-06-28 16:05:01 +0800

    remove javadoc-generated folder from gitignore

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Armor"},{"p":"<Unnamed>","l":"Driver"},{"p":"<Unnamed>","l":"Environment"},{"p":"<Unnamed>","l":"Game"},{"p":"<Unnamed>","l":"Opponent"},{"p":"<Unnamed>","l":"Player"},{"p":"<Unnamed>","l":"Weapon"}];updateSearchResults();
