5620b5e2b6f52635932883b10532b153f4b51070
jncdngs <jonchesterdonguines07@gmail.com>
2025-07-28 23:53:52 +0800

    add javadoc to main gui class

--- src/GUI.java
+++ src/GUI.java
@@ -2,6 +2,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+/**
+ * Main GUI class.
+ * <p>
+ * Initializes and manages the different panels used in the program using a 
+ * main container panel that uses CardLayout.
+ * 
+ * @author Jon Chester Donguines
+ */
 public class GUI extends JFrame {
     
     private CardLayout mainLayout;
@@ -19,6 +27,13 @@ public class GUI extends JFrame {
     
     private ImageIcon icon;
     
+    /**
+     * Constructs a GUI instance.
+     * <p>
+     * Sets the window's name, icon, and dimensions. Sets other properties such as
+     * resizability, position, and close operation. Calls addSetupPanel() which
+     * sets up the panels to be displayed throughout the program.
+     */
     public GUI() {
         // Set window name
         super("Warrior");
@@ -40,6 +55,13 @@ public class GUI extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
+    /**
+     * Initializes and adds all panels to the main container panel using CardLayout.
+     * <p>
+     * This method instantiates each of the custom panel classes (e.g. StartPanel, 
+     * GamePanel), builds them by calling their respective add methods, and adds 
+     * their components to the main panel using string identifiers.
+     */
     public void addSetupPanel() {
         startPanel = new StartPanel();
         gamePanel = new GamePanel();
@@ -86,7 +108,6 @@ public class GUI extends JFrame {
 
         // Winner panel return button
         gamePanel.setBtnReturn(new JButton("Return to main menu"));
-        
         gamePanel.getBtnReturn().setPreferredSize(new Dimension(300, 50));
         gamePanel.getBtnReturn().setFont(new Font("Arial", Font.BOLD, 20));
 
@@ -94,6 +115,14 @@ public class GUI extends JFrame {
         this.add(mainPanel, BorderLayout.CENTER);
     }
 
+    /**
+     * Attaches a shared ActionListener to all JButtons in all panels.
+     * <p>
+     * This method is used by the Controller to centralize event handling.
+     * Each button from each panel is added with the ActionListener.
+     *
+     * @param l the ActionListener that handles button actions
+     */
     public void setActionListeners(ActionListener l) {
         // Add action listeners to start and name panel buttons
         startPanel.getBtnPlay().addActionListener(l);
@@ -125,42 +154,92 @@ public class GUI extends JFrame {
 
 
 
+    /**
+     * Returns the layout of the main panel.
+     * 
+     * @return the layout of the main panel
+     */
     public CardLayout getMainLayout() {
         return this.mainLayout;
     }
     
+    /**
+     * Returns the main container panel.
+     * 
+     * @return the main container panel
+     */
     public JPanel getMainPanel() {
         return this.mainPanel;
     }
 
+    /**
+     * Returns the start menu panel.
+     * 
+     * @return the start menu panel
+     */
     public StartPanel getStartPanel() {
         return this.startPanel;
     }
 
+    /**
+     * Returns the name creation panel.
+     * 
+     * @return the name creation panel
+     */
     public NamePanel getNamePanel() {
         return this.namePanel;
     }
 
+    /**
+     * Returns the armor selection panel.
+     * 
+     * @return the armor selection panel
+     */
     public ArmorPanel getArmorPanel() {
         return this.armorPanel;
     }
 
+    /**
+     * Returns the weapon selection panel.
+     * 
+     * @return the weapon selection panel
+     */
     public WeaponPanel getWeaponPanel() {
         return this.weaponPanel;
     }
 
+    /**
+     * Returns the opponent selection panel.
+     * 
+     * @return the opponent selection panel
+     */
     public OppPanel getOppPanel() {
         return this.oppPanel;
     }
 
+    /**
+     * Returns the environment selection panel.
+     * 
+     * @return the environment selection panel
+     */
     public EnvPanel getEnvPanel() {
         return this.envPanel;
     }
 
+    /**
+     * Returns the main game panel.
+     * 
+     * @return the main game panel
+     */
     public GamePanel getGamePanel() {
         return this.gamePanel;
     }
 
+    /**
+     * Returns the win panel.
+     * 
+     * @return the win panel
+     */
     public WinPanel getWinPanel() {
         return this.winPanel;
     }
